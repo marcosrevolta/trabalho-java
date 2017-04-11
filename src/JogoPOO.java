7fc036eb7c1e982904b86b8120546202bbc1303a
ALAN MARCOS DA SILVA <alanmarcos@univille.br>
2017-04-10 21:54:46 -0300

    xuxu

--- src/JogoPOO.java
+++ src/JogoPOO.java
@@ -14,14 +14,31 @@ public class JogoPOO {
             System.out.println("* 2 - Sair      *");
             System.out.println("*****************");
             opcao = leitor.nextInt();
+            
             if(opcao == 1){
-                int vidas = 1;
-                int pontuacao =0;
+            	
+                int vidas = 3;
+                int pontuacao = 0;
                 int nivel = 1;
                 int rodada = 1;
                 
-                System.out.println("Tentaticas restantes"+vidas+"PontuaÃ§Ã£o"+pontuacao);
+                System.out.println("Tentativas restantes: " + vidas + " // Pontuacao: " + pontuacao);
                 System.out.println("Nivel"+nivel+"Rodada"+rodada);
+                
+                int num1 = gerador.nextInt(50);
+                int num2 = gerador.nextInt(50);
+                int total = num1 + num2;
+                
+                System.out.println(num1 + " + " + num2);
+                int resposta = leitor.nextInt();
+                if(resposta != total){
+                	opcao = 2;
+                } else {
+                	pontuacao += 1;
+                }
+                
+                System.out.println("Pontuação: " + pontuacao);
+                
              }
         }while(opcao != 2);
             System.out.println("Fim do Programa"); 
